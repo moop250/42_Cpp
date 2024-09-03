922252dc15d3e4086b9459439b542deddb6d4806
moop250 <hugo.libine@proton.me>
2024-09-03 17:28:24 +0200

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 16:32:40 by hlibine           #+#    #+#             */
+/*   Updated: 2024/09/03 17:14:36 by hlibine          ###   LAUSANNE.ch       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+
+int	main(int ac, char **av) {
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else {
+		for (int i = 1; av[i]; i++) {
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char) toupper(av[i][j]);
+			if (i < ac - 1)
+				std::cout << ' ';
+		}
+	}
+	std::cout << '\n';
+	return (EXIT_SUCCESS);
+}
