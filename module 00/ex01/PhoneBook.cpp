d97ee975e10cdbb428089d183fae3876d6b95997
moop250 <hugo.libine@proton.me>
2024-09-05 12:17:22 +0200

    qol

--- module 00/ex01/PhoneBook.cpp
+++ module 00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 18:12:01 by hlibine           #+#    #+#             */
-/*   Updated: 2024/09/04 16:08:31 by hlibine          ###   LAUSANNE.ch       */
+/*   Updated: 2024/09/04 17:57:30 by hlibine          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ PhoneBook::~PhoneBook( void ) {
 void PhoneBook::welcome() {
 	std::cout << "\n   Welcome to \"good\" phonebook\n";
 	std::cout << "--------------Usage--------------\n";
-	std::cout << "ADD - Adds a new contact\n";
-	std::cout << "SEARCH - Searches for a contact\n";
-	std::cout << "EXIT - Exits the program\n" << std::endl;
+	std::cout << " ADD - Adds a new contact\n";
+	std::cout << " SEARCH - Searches for a contact\n";
+	std::cout << " EXIT - Exits the program\n" << std::endl;
 }
