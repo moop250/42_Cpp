7033a2b2fedc1892ae381c1a86884f6aa7fd8076
moop250 <hugo.libine@proton.me>
2024-09-12 16:57:06 +0200

    main

--- module 01/ex01/main.cpp
+++ module 01/ex01/main.cpp
@@ -6,8 +6,22 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 14:08:44 by hlibine           #+#    #+#             */
-/*   Updated: 2024/09/12 14:27:04 by hlibine          ###   LAUSANNE.ch       */
+/*   Updated: 2024/09/12 16:55:19 by hlibine          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
 
+
+Zombie*	zombieHorde(int N, std::string name);
+
+int	main(void) {
+	int	n = 10;
+
+	Zombie *tmp = zombieHorde(n, "jeff");
+	for (int i = 0; i < n; ++i)
+		tmp[i].announce();
+
+	delete [] tmp;
+	return 0;
+}
