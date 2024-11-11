952ffe965ad3de210433a2143f091ad10f12cbf6
moop250 <hugo.libine@proton.me>
2024-11-11 12:56:29 +0100

    testing test files

---
+++ module 00/ex00/test.sh
@@ -0,0 +1,6 @@
+make
+printf "\n"
+./megaphone "shhhhh... I think the students are asleep..."
+./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
+./megaphone
+./megaphone "Hello, World!"
