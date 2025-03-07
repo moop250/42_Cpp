812c6419e0e3e92cd7ca6d1ba137be973077fe3b
moop250 <hugo.libine@proton.me>
2025-03-07 17:06:33 +0100

    ex02

--- module 08/ex02/incl/MutantStack.hpp
+++ module 08/ex02/incl/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 17:08:14 by hlibine           #+#    #+#             */
-/*   Updated: 2025/03/06 18:18:43 by hlibine          ###   LAUSANNE.ch       */
+/*   Updated: 2025/03/07 16:55:08 by hlibine          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,25 @@
 
 #include <stack>
 
-template<typename T>
-class MutantStack : public std::stack<T> {
+template<class T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
 	public:
-		MutantStack(void) : std::stack<T>() {};
-		MutantStack(const MutantStack &src) : std::stack<T>(src) {};
-		MutantStack& operator=(const MutantStack &src)
-		{
-			if (this != &src)
-				std::stack<T>::operator=(src);
-			return *this;
+		typedef typename Container::iterator iterator;
+		typedef typename Container::const_iterator const_iterator;
+
+		iterator begin() {
+			return this->c.begin();
 		}
-		~MutantStack();
-		T& top();
+		iterator end() {
+			return this->c.end();
+		}
+	
+		const_iterator begin() const {
+			return this->c.begin();
+		}
+		const_iterator end() const {
+			return this->c.end();
+		}
 };
 
 #endif
--- module 08/ex02/srcs/main.cpp
+++ module 08/ex02/srcs/main.cpp
@@ -6,8 +6,28 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 17:08:17 by hlibine           #+#    #+#             */
-/*   Updated: 2025/03/06 17:32:49 by hlibine          ###   LAUSANNE.ch       */
+/*   Updated: 2025/03/07 17:05:07 by hlibine          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../incl/MutantStack.hpp"
+#include <algorithm>
+#include <iostream>
 
+int	main(void) {
+	MutantStack<int> mutstack;
+
+	mutstack.push(10);
+	mutstack.push(61);
+	mutstack.push(42);
+	mutstack.push(-42);
+	mutstack.push(789);
+	mutstack.push(543);
+
+	std::sort(mutstack.begin(), mutstack.end());
+
+	for (int i = 0; !mutstack.empty(); i++) {
+		std::cout << "Element " << i << ": " << mutstack.top() << std::endl;
+		mutstack.pop();
+	}
+}
