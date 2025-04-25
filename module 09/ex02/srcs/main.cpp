2bf57e3db576c5b8e6040249ec16e0846a2b00e0
moop250 <hugo.libine@proton.me>
2025-04-25 17:43:39 +0200

    temp changes while i build the system for vector

--- module 09/ex02/srcs/main.cpp
+++ module 09/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:57:58 by hlibine           #+#    #+#             */
-/*   Updated: 2025/04/21 14:34:38 by hlibine          ###   ########.fr       */
+/*   Updated: 2025/04/25 17:10:31 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,27 @@
 #include <ctime>
 #include <exception>
 #include <iostream>
-#include <sstream>
-
 
 int	main(int ac, const char *av[]) {
 
 	if (ac == 1) {
 		std::cerr << "Program required input" << std::endl;
+		return 2;
 	}
 
-	std::string	list;
-	clock_t time1, time2;
+	clock_t time1;//, time2;
 
 	try {
-		list = PmergeMe::intake(ac, av);
-		std::cout << "Before: " << list << std::endl;
-		time1 = PmergeMe::sortVector(list);
-		time2 = PmergeMe::sortDeque(list);
-		std::cout << "Time to process a range of " << ac - 1 << "elements with std::PmergeMe::sortVector : " << time1 << " us" << std::endl;
-		std::cout << "Time to process a range of " << ac - 1 << "elements with std::PmergeMe::sortDeque : " << time2 << " us" << std::endl;
+		PmergeMe::intake(ac, av);
+		std::cout << "Before: ";
+		for (int i = 1; i <= ac - 1; i++) {
+			std::cout << av[i] << " ";
+		}
+		std::cout << std::endl;
+		time1 = PmergeMe::sortVector(ac, av);
+		//time2 = PmergeMe::sortDeque(ac, av);
+		std::cout << "Time to process a range of " << ac - 1 << " elements with std::PmergeMe::sortVector : " << time1 << " us" << std::endl;
+//		std::cout << "Time to process a range of " << ac - 1 << "elements with std::PmergeMe::sortDeque : " << time2 << " us" << std::endl;
 	}
 	catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
