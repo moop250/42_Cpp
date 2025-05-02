277efa56081a287f04a53c243060ef332baa78a5
moop250 <hugo.libine@proton.me>
2025-05-02 13:57:00 +0200

    a little bit of reorganisation

--- module 09/ex02/srcs/PmergeMe.cpp
+++ module 09/ex02/srcs/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 17:41:16 by hlibine           #+#    #+#             */
-/*   Updated: 2025/05/01 18:14:13 by hlibine          ###   ########.fr       */
+/*   Updated: 2025/05/02 13:53:07 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,104 +20,6 @@
 #include <sstream>
 #include <stdexcept>
 #include <utility>
-
-template<typename Container>
-TPmergeMe<Container>::TPmergeMe(void) : comparisons_(0), currentJacobsthal(3), prevJacobsthal(1), lvlJacobsthal(4) {};
-
-template<typename Container>
-TPmergeMe<Container>::~TPmergeMe(void) {};
-
-template<typename Container>
-void	TPmergeMe<Container>::swapPairs_(Container &cont, size_t index1, size_t index2) {
-	++comparisons_;
-	if (cont.at(index1) > cont.at(index2))
-		for (size_t i = index2 - index1; i > 0; i--) {
-			std::swap(cont.at(index1--), cont.at(index2--));
-		}
-}
-
-template<typename Container>
-void	TPmergeMe<Container>::moveTo_(Container &src, Container &dst, const size_t len) {
-	for (size_t i = 0; i < len; i++) {
-		dst.push_back(src.at(0));
-		src.erase(src.begin());
-	}
-}
-
-template<typename Container>
-void TPmergeMe<Container>::augmentJacobsthal_() {
-	prevJacobsthal = currentJacobsthal;
-	currentJacobsthal = (std::pow(2,lvlJacobsthal) - pow(-1, lvlJacobsthal)) / 3;
-	++lvlJacobsthal;
-}
-
-template<typename Container>
-void TPmergeMe<Container>::sortCont(const size_t recLev, Container &cont, const int isOdd) {
-	size_t inc = std::pow(2, recLev);
-	size_t inc2 = 2 * inc;
-	/*  Check to see if we have reached the end of the recursion*/
-	if (cont.size() < inc2)
-		return;
-
-	/*  Merge Sort  */
-	size_t limit = cont.size() - inc2 + 1;
-	for (size_t i = 0; i < limit; i += inc2) {
-		size_t index1 = i + inc - 1;
-		size_t index2 = i + inc2 - 1;
-		swapPairs_(cont, index1, index2);
-	}
-	TPmergeMe<Container>::sortCont(recLev + 1, cont, isOdd);
-
-	/*  Split numbers into main and pend  */
-	for (size_t i = 0; i < inc2; i++) {
-		main_.push_back(cont.at(0));
-		cont.erase(cont.begin());
-	}
-
-	for (size_t i = 0, b = 0; i + inc < limit; i += inc) {
-		if (++b % 2)
-			moveTo_(cont, pend_, inc);
-		else
-			moveTo_(cont, main_, inc);
-	}
-	moveTo_(cont, non_, cont.size());
-
-/* 	std::cout << "main: ";
-	for (size_t i = 0; i < main_.size(); i++) {
-		std::cout << main_.at(i) << " ";
-	}
-	std::cout << std::endl;
-
-	std::cout << "pend: ";
-	for (size_t i = 0; i < pend_.size(); i++) {
-		std::cout << pend_.at(i) << " ";
-	}
-	std::cout << std::endl;
-
-	std::cout << "non: ";
-	for (size_t i = 0; i < non_.size(); i++) {
-		std::cout << non_.at(i) << " ";
-	}
-	std::cout << std::endl << std::endl;
- */
-	/*  Insertion Sort  */
-	size_t jacobsthalDiff = currentJacobsthal - prevJacobsthal;
-	size_t count = jacobsthalDiff;
-	while (jacobsthalDiff--)
-		if (jacobsthalDiff > pend_.size())
-			break;
-		size_t bin = binarySearch_(pend_.at((count * inc) - 1), pow(2, (lvlJacobsthal + 1) - 1), inc);
-	}
-
-	augmentJacobsthal_();
-
-	/*  Push everything back into cont  */
-	moveTo_(main_, cont, main_.size());
-	moveTo_(pend_, cont, pend_.size());
-	moveTo_(non_, cont, non_.size());
-
-	return ;
-}
 
 void PmergeMe::intake(int ac, const char **av) {
 	long		num;
@@ -150,7 +52,7 @@ double PmergeMe::sortVector(const int ac, const char **av) {
 	// start a timer
 	clock_t	timer_start = clock();
 
-	visort.sortCont(0, vec, (ac - 1) % 2);
+	visort.sortCont(0, vec, vec.size() % 2);
 
 	// save time it took to complete operation
 	clock_t	timer_end = clock();
--- module 09/ex02/srcs/main.cpp
+++ module 09/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:57:58 by hlibine           #+#    #+#             */
-/*   Updated: 2025/04/30 10:42:50 by hlibine          ###   ########.fr       */
+/*   Updated: 2025/05/02 13:56:04 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int ac, const char *av[]) {
 		return 2;
 	}
 
-	clock_t time1;//, time2;
+	clock_t time1, time2;
 
 	try {
 		PmergeMe::intake(ac, av);
@@ -35,10 +35,15 @@ int	main(int ac, const char *av[]) {
 		}
 		std::cout << std::endl;
 		time1 = PmergeMe::sortVector(ac, av);
-		//time2 = PmergeMe::sortDeque(ac, av);
-		std::cout << std::setprecision(6);
-		std::cout << "Time to process a range of " << ac - 1 << " elements with std::PmergeMe::sortVector : " << time1 << " us" << std::endl;
-//		std::cout << "Time to process a range of " << ac - 1 << "elements with std::PmergeMe::sortDeque : " << time2 << " us" << std::endl;
+		time2 = PmergeMe::sortDeque(ac, av);
+		std::cout << "Time to process a range of "
+			<< ac - 1 << " elements with std::PmergeMe::sortVector : "
+			<< std::fixed << std::setprecision(6) << time1
+			<< " us" << std::endl;
+		std::cout << "Time to process a range of "
+			<< ac - 1 << " elements with std::PmergeMe::sortDeque : "
+			<< std::fixed << std::setprecision(6) << time2
+			<< " us" << std::endl;
 	}
 	catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
