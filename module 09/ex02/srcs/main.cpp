748b963f76882062d91e2c01cdded0d7522046d4
moop250 <hugo.libine@proton.me>
2025-05-08 14:22:40 +0200

    final touches for pmergeme

--- module 09/ex02/srcs/main.cpp
+++ module 09/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:57:58 by hlibine           #+#    #+#             */
-/*   Updated: 2025/05/07 16:46:55 by hlibine          ###   ########.fr       */
+/*   Updated: 2025/05/08 14:16:15 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,13 @@ int	main(int ac, const char *av[]) {
 		time1 = PmergeMe::sortVector(ac, av);
 		time2 = PmergeMe::sortDeque(ac, av);
 		std::cout << "Time to process a range of "
-			<< ac - 1 << " elements with std::PmergeMe::sortVector :\t"
-			<< std::fixed << std::setprecision(6) << (double)time1 / CLOCKS_PER_SEC * 1000
-			<< " us" << std::endl;
+			<< C_CYAN << ac - 1 << C_RESET << " elements with std::PmergeMe::sortVector :\t"
+			<< std::fixed << std::setprecision(6) << C_CYAN << (double)time1 / CLOCKS_PER_SEC * 1000
+			<< C_RESET << " us" << std::endl;
 		std::cout << "Time to process a range of "
-			<< ac - 1 << " elements with std::PmergeMe::sortDeque :\t"
-			<< std::fixed << std::setprecision(6) << (double)time2 / CLOCKS_PER_SEC * 1000
-			<< " us" << std::endl;
+			<< C_CYAN << ac - 1 << C_RESET << " elements with std::PmergeMe::sortDeque :\t"
+			<< std::fixed << std::setprecision(6) << C_CYAN << (double)time2 / CLOCKS_PER_SEC * 1000
+			<< C_RESET << " us" << std::endl;
 	}
 	catch (std::exception &e) {
 		std::cerr << C_RED << "Error: " << e.what() << C_RESET << std::endl;
--- module 09/ex02/tester.sh
+++ module 09/ex02/tester.sh
@@ -8,8 +8,14 @@ if ! [ -x "./PmergeMe" ]; then
     exit 1
 fi
 
+echo "15 in order"
 ./PmergeMe 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 | grep Time && echo ""
 
+echo "22 out of order"
 ./PmergeMe 2 11 0 17 6 15 8 16 3 10 1 21 9 18 14 19 5 12 4 20 7 13 | grep Time && echo ""
 
+echo "100 random"
+./PmergeMe $(shuf -i 1-1000 -n 100 | tr '\n' ' ') | grep Time && echo ""
+
+echo "3000 random"
 ./PmergeMe $(shuf -i 1-100000 -n 3000 | tr '\n' ' ') | grep Time && echo ""
